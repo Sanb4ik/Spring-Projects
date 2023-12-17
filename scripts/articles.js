b6707b0afcf4dbfdae8ad3cb854af91605c7d376
Sanb4ik <sashabelaz7@gmail.com>
2023-12-17 20:19:20 +0300

    deleted insertAdjacentHTML

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,25 +1,90 @@
 import { articlesData } from "./constants.js";
 
+// function createArticles(articles) {
+//   const list = document.getElementById("list");
+//   articles.forEach((item) => {
+//     const html = `
+//       <article class="article">
+//         <a class="article-item" href="https://spring.io/projects/${item.img}">
+//           <img
+//             class="article-item__img"
+//             src="https://spring.io/img/projects/${item.img}.svg"
+//             alt="${item.title}"
+//           />
+//           <div class="article-item__info">
+//             <h1 class="article-item__h1">${item.title}</h1>
+//             <p class="article-item__p">${item.description}</p>
+//           </div>
+//         </a>
+//       </article>
+//     `;
+//
+//     list.insertAdjacentHTML("beforeend", html);
+//   });
+// }
+//
+// export default function filterArticles(userInput) {
+//   const filteredArticles = articlesData.filter((item) => {
+//     return (
+//       item.description.toLowerCase().includes(userInput.toLowerCase()) ||
+//       item.title.toLowerCase().includes(userInput.toLowerCase())
+//     );
+//   });
+//   updateArticles(filteredArticles.length > 0 ? filteredArticles : null);
+// }
+//
+// function updateArticles(articles) {
+//   const list = document.getElementById("list");
+//   list.innerHTML = "";
+//
+//   if (articles) {
+//     createArticles(articles);
+//   } else {
+//     list.textContent = "No results";
+//   }
+// }
+//
+// createArticles(articlesData);
+
+function createArticleElement(item) {
+  const article = document.createElement("article");
+  article.className = "article";
+
+  const link = document.createElement("a");
+  link.className = "article-item";
+  link.href = `https://spring.io/projects/${item.img}`;
+
+  const img = document.createElement("img");
+  img.className = "article-item__img";
+  img.src = `https://spring.io/img/projects/${item.img}.svg`;
+  img.alt = item.title;
+  link.appendChild(img);
+
+  const infoDiv = document.createElement("div");
+  infoDiv.className = "article-item__info";
+
+  const h1 = document.createElement("h1");
+  h1.className = "article-item__h1";
+  h1.textContent = item.title;
+
+  const p = document.createElement("p");
+  p.className = "article-item__p";
+  p.textContent = item.description;
+
+  infoDiv.appendChild(h1);
+  infoDiv.appendChild(p);
+
+  link.appendChild(infoDiv);
+  article.appendChild(link);
+
+  return article;
+}
+
 function createArticles(articles) {
   const list = document.getElementById("list");
   articles.forEach((item) => {
-    const html = `
-      <article class="article">
-        <a class="article-item" href="https://spring.io/projects/${item.img}">
-          <img
-            class="article-item__img"
-            src="https://spring.io/img/projects/${item.img}.svg"
-            alt="${item.title}"
-          />
-          <div class="article-item__info">
-            <h1 class="article-item__h1">${item.title}</h1>
-            <p class="article-item__p">${item.description}</p>
-          </div>
-        </a>
-      </article>
-    `;
-
-    list.insertAdjacentHTML("beforeend", html);
+    const articleElement = createArticleElement(item);
+    list.appendChild(articleElement);
   });
 }
 
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,53 +1,133 @@
 import { menuItems } from "./constants.js";
 
+// function generateDropDownItems(items, liClass, aClass) {
+//   return items.map(
+//     (item) =>
+//       `<li class=${liClass}><a class=${aClass} href="${item.href}">${item.text}</a></li>`
+//   ).join("");
+// }
+//
+// function createDropdownMenu(menuData) {
+//   return `
+//     <div class="dropdown">
+//       <button class="drop-btn">${menuData.title}</button>
+//       <i class="arrow down"></i>
+//       <ul class="dropdown-content">
+//         ${generateDropDownItems(menuData.items, "", "dropdown-item" )}
+//       </ul>
+//     </div>
+//   `;
+// }
+//
+// function createDropdownMenuForMobile(menuData) {
+//   return `
+//             <article class="menu-item">
+//               <div class="menu-item__container">
+//                 <h1 class="menu-item__h1">${menuData.title}</h1>
+//                 <button class="menu-item__arrow down"></button>
+//               </div>
+//               <ul class="dropdown-m">
+//                 ${generateDropDownItems(menuData.items, "my-3", "dropdown-m__item" )}
+//               </ul>
+//             </article>
+//   `;
+// }
+//
+// function createMobileMenu() {
+//   const _menu = document.getElementById("menu-content");
+//   menuItems.forEach((menu) => {
+//     const dropdownHTML = createDropdownMenuForMobile(menu);
+//     _menu.insertAdjacentHTML("beforeend", dropdownHTML);
+//   });
+// }
+//
+// createMobileMenu();
+//
+// const dropdownContainer = document.getElementById("navbar-menu");
+// menuItems.forEach((menu) => {
+//   const dropdownHTML = createDropdownMenu(menu);
+//   dropdownContainer.insertAdjacentHTML("beforeend", dropdownHTML);
+// });
+
 function generateDropDownItems(items, liClass, aClass) {
-  return items.map(
-    (item) =>
-      `<li class=${liClass}><a class=${aClass} href="${item.href}">${item.text}</a></li>`
-  ).join("");
+  return items.map((item) => {
+    const li = document.createElement("li");
+    li.className = liClass;
+    const a = document.createElement("a");
+    a.className = aClass;
+    a.href = item.href;
+    a.textContent = item.text;
+    li.appendChild(a);
+    return li;
+  });
 }
 
 function createDropdownMenu(menuData) {
-  return `
-    <div class="dropdown">
-      <button class="drop-btn">${menuData.title}</button>
-      <i class="arrow down"></i>
-      <ul class="dropdown-content">
-        ${generateDropDownItems(menuData.items, "", "dropdown-item" )}
-      </ul>
-    </div>
-  `;
+  const dropdown = document.createElement("div");
+  dropdown.className = "dropdown";
+
+  const button = document.createElement("button");
+  button.className = "drop-btn";
+  button.textContent = menuData.title;
+  dropdown.appendChild(button);
+
+  const arrow = document.createElement("i");
+  arrow.className = "arrow down";
+  dropdown.appendChild(arrow);
+
+  const ul = document.createElement("ul");
+  ul.className = "dropdown-content";
+  const items = generateDropDownItems(menuData.items, "", "dropdown-item");
+  items.forEach((item) => ul.appendChild(item));
+  dropdown.appendChild(ul);
+
+  return dropdown;
 }
 
 function createDropdownMenuForMobile(menuData) {
-  return `
-            <article class="menu-item">
-              <div class="menu-item__container">
-                <h1 class="menu-item__h1">${menuData.title}</h1>
-                <button class="menu-item__arrow down"></button>
-              </div>
-              <ul class="dropdown-m">
-                ${generateDropDownItems(menuData.items, "my-3", "dropdown-m__item" )}
-              </ul>
-            </article>
-  `;
+  const article = document.createElement("article");
+  article.className = "menu-item";
+
+  const container = document.createElement("div");
+  container.className = "menu-item__container";
+  article.appendChild(container);
+
+  const h1 = document.createElement("h1");
+  h1.className = "menu-item__h1";
+  h1.textContent = menuData.title;
+  container.appendChild(h1);
+
+  const button = document.createElement("button");
+  button.className = "menu-item__arrow down";
+  container.appendChild(button);
+
+  const ul = document.createElement("ul");
+  ul.className = "dropdown-m";
+  const items = generateDropDownItems(menuData.items, "my-3", "dropdown-m__item");
+  items.forEach((item) => ul.appendChild(item));
+  article.appendChild(ul);
+
+  return article;
 }
 
 function createMobileMenu() {
-  const _menu = document.getElementById("menu-content");
+  const menuContent = document.getElementById("menu-content");
   menuItems.forEach((menu) => {
-    const dropdownHTML = createDropdownMenuForMobile(menu);
-    _menu.insertAdjacentHTML("beforeend", dropdownHTML);
+    const dropdown = createDropdownMenuForMobile(menu);
+    menuContent.appendChild(dropdown);
+  });
+}
+
+function createMenu() {
+  const navbarMenu = document.getElementById("navbar-menu");
+  menuItems.forEach((menu) => {
+    const dropdown = createDropdownMenu(menu);
+    navbarMenu.appendChild(dropdown);
   });
 }
 
 createMobileMenu();
-
-const dropdownContainer = document.getElementById("navbar-menu");
-menuItems.forEach((menu) => {
-  const dropdownHTML = createDropdownMenu(menu);
-  dropdownContainer.insertAdjacentHTML("beforeend", dropdownHTML);
-});
+createMenu();
 
 const burgerBtn = document.getElementById("hm-menu");
 const closeBtn = document.getElementById("close-menu");
